ae4cbb81842097eb822320bccabc5ed808d6f0d6
chaopaoo12 <chaopaoo12@hotmail.com>
2024-08-02 10:31:34 +0800

    new file:   crawl/thingiverse.com/__pycache__/crawl_list.cpython-311.pyc 	modified:   crawl/thingiverse.com/crawl_list.py 	new file:   crawl/thingiverse.com/test.ipynb 	new file:   crawl/thingiverse.com/ttt.ipython

--- crawl/thingiverse.com/crawl_list.py
+++ crawl/thingiverse.com/crawl_list.py
@@ -4,54 +4,164 @@ import requests
 import json
 import datetime
 import time
-
-def get_headers(headers):
-    url = 'https://www.thingiverse.com/'
-    options = webdriver.ChromeOptions()
-    # 设置中文
-    for (key,value) in headers.items():
-        options.add_argument('%s="%s"' % (key, value))
-    options.add_argument('headless')
-    driver = webdriver.Chrome(chrome_options=options)
-    driver.get(url)
-    cookies=driver.get_cookies()
-    driver.quit()
-    ck = dict()
-    for i in cookies:
-        ck[i['name']] = i['value']
-    headers.update({"Cookie":' ;'.join([k+'='+v for (k,v) in ck.items()])})
-    return(headers)
+from bs4 import BeautifulSoup
 
 class web_d():
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self,headers) -> None:
+        self.headers = headers
 
-    def get_headers(headers):
+    def get_headers(self):
         url = 'https://www.thingiverse.com/'
-        options = webdriver.ChromeOptions()
-        # 设置中文
-        for (key,value) in headers.items():
-            options.add_argument('%s="%s"' % (key, value))
-        options.add_argument('headless')
-        driver = webdriver.Chrome(chrome_options=options)
+        self.options = webdriver.ChromeOptions()
+        for (key,value) in self.headers.items():
+            self.options.add_argument('%s="%s"' % (key, value))
+        self.options.add_argument('headless')
+        driver = webdriver.Chrome(chrome_options=self.options)
         driver.get(url)
         cookies=driver.get_cookies()
         driver.quit()
         ck = dict()
         for i in cookies:
             ck[i['name']] = i['value']
-        headers.update({"Cookie":' ;'.join([k+'='+v for (k,v) in ck.items()])})
-        return(headers)
+        self.headers.update({"Cookie":' ;'.join([k+'='+v for (k,v) in ck.items()])})
 
-def get_category(web_d, url):
+    def get_options(self,ip_proxy=None):
+        self.options = webdriver.chrome.options.Options()
+        for (key,value) in self.headers.items():
+            self.options.add_argument('%s="%s"' % (key, value))
+        self.options.add_argument('headless')
+        self.options.add_argument("--no-sandbox")
+        self.options.add_argument('--disable-gpu')
+        self.options.add_argument("--disable-dev-shm-usage")
+        self.options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        self.options.add_experimental_option('useAutomationExtension', False)
+        if ip_proxy is not None:
+            self.options.add_argument(ip_proxy)
+
+    def get_driver(self,ip_proxy=None):
+        self.get_options(ip_proxy)
+        driver = webdriver.Chrome(options=self.options)
+        driver.get('https://www.thingiverse.com/')
+        cookies=driver.get_cookies()
+        driver.quit()
+        ck = dict()
+        for i in cookies:
+            ck[i['name']] = i['value']
+
+        self.headers.update({"Cookie":' ;'.join([k+'='+v for (k,v) in ck.items()])})
+        option = self.get_options(ip_proxy)
+        self.driver = webdriver.Chrome(options=option)
+        return(self.driver)
+
+    
+def get_soup(driver):
+    soup = BeautifulSoup(driver.page_source, "html.parser").body
+    #k = 1
+    #while soup is None or soup.text == 'Request was throttled. Please wait a moment and refresh the page':
+    #    print('get soup {} times.'.format(k))
+    #    k += 1
+    #    driver.refresh()
+    #    time.sleep(3)
+    #    soup = BeautifulSoup(driver.page_source, "html.parser").body
+    return(soup)
+
+def read_data_from(driver, url):
+    print('read data from:', url.encode('ascii', 'ignore').decode('unicode_escape'))
+    driver.get(url.encode('ascii', 'ignore').decode('unicode_escape'))
+    # if url is not None:
+    #     try:
+    #         driver.get(url.encode('ascii', 'ignore').decode('unicode_escape'))
+    #         #element = EC.url_changes(url)
+    #         #try:
+    #         #    WebDriverWait(driver, 5).until(element)
+    #         #    flag1 = element(driver)
+    #         #except:
+    #         #    flag1 = element(driver)
+
+    #         #if flag1
+    #         #driver.implicitly_wait(10)
+    #     except:
+    #         print(url)
+    return(driver)
+
+def get_info_listPage(html):
+    docs = html.find_all(class_='item-card-container')
+
+    return([doc.find(class_='ItemCardContent__itemCardUserLink--gMgsV').text  
+            if doc.find(class_='ItemCardContent__itemCardUserLink--gMgsV') is not None else None for doc in docs],
+           [doc.find(class_='ItemCardContent__itemCardUserLink--gMgsV').get('href') 
+            if doc.find(class_='ItemCardContent__itemCardUserLink--gMgsV') is not None else None for doc in docs],
+           [doc.find(class_='ItemCardHeader__itemCardHeader--cPULo').get('href') 
+            if doc.find(class_='ItemCardHeader__itemCardHeader--cPULo') is not None else None for doc in docs],
+           [doc.find(class_='ItemCardHeader__itemCardHeader--cPULo').text 
+            if doc.find(class_='ItemCardHeader__itemCardHeader--cPULo') is not None else None for doc in docs],
+           [doc.find(class_='ItemCardHeader__itemCardHeader--cPULo').get('href') 
+            if doc.find(class_='ItemCardHeader__itemCardHeader--cPULo') is not None else None for doc in docs]
+           #[doc.find(class_='ItemCardHeader__itemCardHeader--cPULo').get('href') for doc in docs],
+           #[doc.find('div',{'class':['_cDEzb_p13n-sc-css-line-clamp-3_g3dy1',
+           #                         '_cDEzb_p13n-sc-css-line-clamp-4_2q2cc']}).text if doc.find('div',{'class':['_cDEzb_p13n-sc-css-line-clamp-3_g3dy1','_cDEzb_p13n-sc-css-line-clamp-4_2q2cc']}) is not None else None for doc in docs],
+           #[doc.find(attrs={'class':'a-link-normal','tabindex':'-1'}).get('href') if doc.find(attrs={'class':'a-link-normal','tabindex':'-1'}) is not None else None for doc in docs],
+           #[doc.find(class_='p13n-sc-uncoverable-faceout').get('id') if doc.find(class_='p13n-sc-uncoverable-faceout') is not None else None for doc in docs],
+           #[doc.find(class_='a-icon-alt').text.replace(' out of 5 stars','') if doc.find(class_='a-icon-alt') is not None else None for doc in docs],
+           #[doc.find(class_='a-size-small').text.replace(',','') if doc.find(class_='a-size-small') is not None else 0 for doc in docs],
+           #[doc.find(class_='a-size-base').text.replace('$','').replace('\xa0','') if doc.find(class_='a-size-base') is not None else 0 for doc in docs]
+           )
+
+def read_info_listPage(html):
+    username, userlink, itemlink, itemtitle, itemid = get_info_listPage(html)
+    #, comments, price 
+    temp = pd.DataFrame({'username':username,
+                         'userlink':userlink,
+                         'itemlink': itemlink,
+                         'itemtitle': itemtitle,
+                         'itemid': itemid
+                         #'itemjpg': href,
+                         #'itemlike': star,
+                         #'itemcollect': comments,
+                         #'itemcomments': price
+                         })
+    return(temp)
+
+
+def get_info_itemPage(html):
+    docs = html.find_all(class_='DetailPageTitle__thingTitleMeta--P5VUn') #上架日期
+
+    docs = html.find_all(class_='TagList__tagList--DkseJ').find_all(class_='Button__buttonContent--AZZB4 button-content').text #tags
+
+    docs = html.find_all(class_='License__licenseText--iIyuZ').find_all(class_='Button__buttonContent--AZZB4 button-content') #license
     pass
 
-def read_list(web_d, url):
-    pass
+    #return([doc.find(class_='ItemCardContent__itemCardUserLink--gMgsV').text for doc in docs],
+    #       [doc.find(class_='ItemCardContent__itemCardUserLink--gMgsV').get('href') for doc in docs],
+    #       [doc.find(class_='ItemCardHeader__itemCardHeader--cPULo').get('href') for doc in docs],
+    #       [doc.find(class_='ItemCardHeader__itemCardHeader--cPULo').text for doc in docs],
+    #       [doc.find(class_='ItemCardHeader__itemCardHeader--cPULo').get('href') for doc in docs]
+           #[doc.find(class_='ItemCardHeader__itemCardHeader--cPULo').get('href') for doc in docs],
+           #[doc.find('div',{'class':['_cDEzb_p13n-sc-css-line-clamp-3_g3dy1',
+           #                         '_cDEzb_p13n-sc-css-line-clamp-4_2q2cc']}).text if doc.find('div',{'class':['_cDEzb_p13n-sc-css-line-clamp-3_g3dy1','_cDEzb_p13n-sc-css-line-clamp-4_2q2cc']}) is not None else None for doc in docs],
+           #[doc.find(attrs={'class':'a-link-normal','tabindex':'-1'}).get('href') if doc.find(attrs={'class':'a-link-normal','tabindex':'-1'}) is not None else None for doc in docs],
+           #[doc.find(class_='p13n-sc-uncoverable-faceout').get('id') if doc.find(class_='p13n-sc-uncoverable-faceout') is not None else None for doc in docs],
+           #[doc.find(class_='a-icon-alt').text.replace(' out of 5 stars','') if doc.find(class_='a-icon-alt') is not None else None for doc in docs],
+           #[doc.find(class_='a-size-small').text.replace(',','') if doc.find(class_='a-size-small') is not None else 0 for doc in docs],
+           #[doc.find(class_='a-size-base').text.replace('$','').replace('\xa0','') if doc.find(class_='a-size-base') is not None else 0 for doc in docs]
+    #       )
 
-def read_itempage(web_d, url):
-    pass
+def read_info_itemPage(html):
+    username, userlink, itemlink, itemtitle, itemid = get_info_listPage(html)
+    #, comments, price 
+    temp = pd.DataFrame({'username':username,
+                         'userlink':userlink,
+                         'itemlink': itemlink,
+                         'itemtitle': itemtitle,
+                         'itemid': itemid
+                         #'itemjpg': href,
+                         #'itemlike': star,
+                         #'itemcollect': comments,
+                         #'itemcomments': price
+                         })
+    return(temp)
+
 
 def read_userpage(web_d, url):
     pass
@@ -59,49 +169,29 @@ def read_userpage(web_d, url):
 def download_files(web_d, url):
     pass
 
-def read_data_from_xueqiu(url, headers, proxy=None):
 
-    url = url.format(timestamp = int(time.mktime(datetime.datetime.now().timetuple())*1000))
-    if proxy is not None:
-        proxies = {
-            "http": "http://" + proxy[0],
-        }
-        response = requests.get(url,headers=headers, proxies=proxies)
-    else:
-        response = requests.get(url,headers=headers)
-    res_dict = json.loads(response.text)
+def get_product_info_listpage(url, driver):
 
-    if res_dict['data'] is not None:
-        try:
-            data = pd.DataFrame(res_dict['data']['item'],columns=res_dict['data']['column']).assign(code = res_dict['data']['symbol'])
-        except:
-            data = None
-    else:
-        print('fail')
-        data = None
-    return(data)
+    driver = read_data_from(driver, url)
+    print('load url success')
+    html = get_soup(driver)
+    print('bs4 success')
+    temp = read_info_listPage(html)
+    print('read html success')
+    driver.quit()
+    return(temp)
 
 
-def read_financial_report(code, exchange, report_type):
-    stockfi_url = 'https://stock.xueqiu.com/v5/stock/finance/{exchange}/{report_type}.json?symbol={code}&type=all&is_detail=true&count=1000&timestamp={timestamp}'.format(code=code,exchange=exchange, report_type=report_type, timestamp= int(time.mktime(datetime.datetime.now().timetuple())*1000))
-    data = read_data_from_xueqiu(stockfi_url)
-    columns_list = []
-    for i in data['data']['list']:
-        columns_list = columns_list +list(data['data']['list'][0].keys())
-    columns_list = list(set(columns_list))
-    if report_type != 'income':
-        columns_list.remove('ctime')
-        columns_list.remove('sd')
-        columns_list.remove('ed')
-    res = pd.DataFrame()
-    for j in data['data']['list']:
-        a = dict()
-        for i in columns_list:
-            if isinstance(j[i],list):
-                a[i] = j[i][0]
-            else:
-                a[i] = j[i]
-        res = res.append(pd.DataFrame(a,index=[0]))
-    res = res.assign(report_date = res.report_date.apply(lambda x:x/1000))
-    res = res.assign(report_date = res.report_date.apply(lambda x:str(datetime.datetime.fromtimestamp(x))[0:10]))
-    return(res)
+if __name__ == '__main__':
+    url = 'https://www.thingiverse.com/?page=1&sort=newest'
+    headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+               'Accept-Language': 'zh-CN,zh;q=0.9',
+               'Cache-Control': 'max-age=0',
+               'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36',
+               'Connection': 'close'
+               }
+    driver = web_d(headers)
+    driver = driver.get_driver()
+    data = get_product_info_listpage(url,driver)
+    driver.quit()
+    print(data)
